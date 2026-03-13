5924c127935d2b88c18e02d0bcbb80c3c69a088f
TUYIZERE Niyogisubizo Abdon <abdontuyizere@gmail.com>
2026-03-13 22:26:38 +0200

    corrections

---
+++ scripts/prophets.js
@@ -0,0 +1,38 @@
+const url = 'https://byui-cse.github.io/cse-ww-program/data/latter-day-prophets.json';
+
+const cards = document.querySelector('#cards');
+
+async function getProphetData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayProphets(data.prophets);
+}
+
+getProphetData();
+
+displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+        let card = document.createElement('section');
+        let h2 = document.createElement('h2');
+        let portrait = document.createElement('img');
+        let birthdate = document.createElement('p');
+        let birthplace = document.createElement('p');
+
+        h2.textContent = `${prophet.name} ${prophet.lastname}`;
+        birthdate.textContent = `Date of Birth: ${prophet.birthdate}`;
+        birthplace.textContent = `Place of Birth: ${prophet.birthplace}`;
+
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '200');
+        portrait.setAttribute('height', '200');
+
+        card.appendChild(h2);
+        card.appendChild(birthdate);
+        card.appendChild(birthplace);
+        card.appendChild(portrait);
+
+        cards.appendChild(card);
+    });
+}
